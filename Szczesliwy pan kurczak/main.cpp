2c31f85fecf926e84b9a8c57675cd44c89e37f3d
roktaba <r.oktaba@gmail.com>
2019-03-10 23:50:51 +0100

    Screen

--- Szczesliwy pan kurczak/main.cpp
+++ Szczesliwy pan kurczak/main.cpp
@@ -1,5 +1,7 @@
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::RenderWindow(sf::VideoMode(800, 640), "Sczesliwy pan kurczak", sf::Style::Titlebar | sf::Style::Close | sf::Style::Fullscreen);
 	return 0;
 }
